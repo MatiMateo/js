18415bbe751d14b7a603c8300a54df24cd62f0be
Matías Scavuzzo <mati.ntvg@hotmail.com>
2022-06-22 21:07:01 -0300

    Subiendo todo

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let edad = parseInt(prompt("Ingrese su edad"));
+if (edad >= 18){
+     salida = "Bienvenido";
+} else {
+      salida = "Necesitas ser mayor de 18 años para ingresar al sitio"
+};
+alert(salida);
