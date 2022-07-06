7d9275568ff7632624e17e05abbae8b80c0ffaec
Matías Scavuzzo <mati.ntvg@hotmail.com>
2022-07-05 21:28:21 -0300

    Añadi funciones

--- scripts/index.js
+++ scripts/index.js
@@ -5,28 +5,40 @@ let dia4 = "Jueves";
 let dia5 = "Viernes"; 
 let dia6 = "Sábado"; 
 let turnos = 0;
+const lunes = (name) => alert(`Bienvenide ${name} su turno es para el ${dia1}`)
+const martes = (name) => alert(`Bienvenide ${name} su turno es para el ${dia2}`)
+const miercoles = (name) => alert(`Bienvenide ${name} su turno es para el ${dia3}`)
+const jueves = (name) => alert(`Bienvenide ${name} su turno es para el ${dia4}`)
+const viernes = (name) => alert(`Bienvenide ${name} su turno es para el ${dia5}`)
+const sabado = (name) => alert(`Bienvenide ${name} su turno es para el ${dia6}`)
 while (turnos <= 6){
-      prompt("Ingrese su nombre").toLowerCase();
+      const nombre = prompt("Ingrese su nombre");
+      const name = nombre[0].toUpperCase() + nombre.substring(1);
       if (turnos == 0) { 
-            alert("Su turno es para el " + dia1);
+            lunes(name);
             turnos++;
       } else if (turnos == 1) {
-            alert("Su turno es para el " + dia2);
+            martes(name);
             turnos++;
       } else if (turnos == 2) {      
-            alert("Su turno es para el " + dia3);
+            miercoles(name);
             turnos++;
       } else if (turnos == 3) {
-            alert("Su turno es para el " + dia4);
+            jueves(name);
             turnos++;
       } else if (turnos == 4) {
-            alert("Su turno es para el " + dia5);
+            viernes(name);
             turnos++;
       } else if (turnos == 5){
-            alert("Su turno es para el " + dia6);
+            sabado(name);
             turnos++;
       } else {
             alert("No hay más turnos disponibles por el momento");
             break;
       }
 }
+
+/* const nombre = prompt("Ingrese su nombre");
+const resultado = nombre[0].toUpperCase() + nombre.substring(1);
+alert(resultado); */
+
