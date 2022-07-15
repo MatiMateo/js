d6ffd29751ad857723e9dbbb097e0150cce32347
Matías Scavuzzo <mati.ntvg@hotmail.com>
2022-07-14 23:16:43 -0300

    Interacción catálogo-carrito

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,28 @@
-let dia1 = "Lunes"; 
-let dia2 = "Martes"; 
-let dia3 = "Miércoles"; 
-let dia4 = "Jueves"; 
-let dia5 = "Viernes"; 
-let dia6 = "Sábado"; 
-let turnos = 0;
-const lunes = (name) => alert(`Bienvenide ${name} su turno es para el ${dia1}`)
-const martes = (name) => alert(`Bienvenide ${name} su turno es para el ${dia2}`)
-const miercoles = (name) => alert(`Bienvenide ${name} su turno es para el ${dia3}`)
-const jueves = (name) => alert(`Bienvenide ${name} su turno es para el ${dia4}`)
-const viernes = (name) => alert(`Bienvenide ${name} su turno es para el ${dia5}`)
-const sabado = (name) => alert(`Bienvenide ${name} su turno es para el ${dia6}`)
-while (turnos <= 6){
-      const nombre = prompt("Ingrese su nombre");
-      const name = nombre[0].toUpperCase() + nombre.substring(1);
-      if (turnos == 0) { 
-            lunes(name);
-            turnos++;
-      } else if (turnos == 1) {
-            martes(name);
-            turnos++;
-      } else if (turnos == 2) {      
-            miercoles(name);
-            turnos++;
-      } else if (turnos == 3) {
-            jueves(name);
-            turnos++;
-      } else if (turnos == 4) {
-            viernes(name);
-            turnos++;
-      } else if (turnos == 5){
-            sabado(name);
-            turnos++;
-      } else {
-            alert("No hay más turnos disponibles por el momento");
-            break;
+//Creación de interacción entre catálogo y carrito
+class Producto{
+      constructor (nombre, precio, cantidad) {
+            this.nombre = nombre;
+            this.precio = parseFloat(precio);
+            this.cantidad = parseInt(cantidad);
+      }
+      agregarAlCarrito (item, cant) {
+            let index = catalogo.indexOf(item);
+            if (index != -1 && this.cantidad > cant) {
+                  this.cantidad = this.cantidad - cant;
+                  carrito.push(item);
+            } else {
+                  console.log("No hay más productos disponibles");
+            }
       }
 }
-
-/* const nombre = prompt("Ingrese su nombre");
-const resultado = nombre[0].toUpperCase() + nombre.substring(1);
-alert(resultado); */
-
+catalogo = [];
+carrito =[];
+const remeras = new Producto("Remeras","1800","10");
+const jeans = new Producto("Jeans", "5200", "15");
+const sweaters = new Producto("Sweaters", "3350", "5");
+const cintos = new Producto("Cintos", "950", "20");
+catalogo.push(remeras, jeans, sweaters, cintos);
+console.log(catalogo);
+remeras.agregarAlCarrito(remeras, 4);
+console.log(catalogo);
+console.log(carrito);
